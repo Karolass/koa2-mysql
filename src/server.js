8c81a2bfaccfef3476a717d758e1af7b9e97d315
KaroKuo <karo.k@tc-gaming.co>
2019-06-17 19:04:32 +0800

    [modify] model.test - model test coverage 100%

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from './app'
+import { sequelize, testDBConnection } from './models'
 import Config from './config'
 
 app.listen(Config.port, () => {
   console.log(`Server start on port ${Config.port}...`)
+  testDBConnection(sequelize)
 })
